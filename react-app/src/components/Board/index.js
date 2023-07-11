7685c10c6bb5a7e2cd6f352b72b26d307fa3e6a0
jwily <johnlee1120@gmail.com>
2023-07-10 17:42:01 -0700

    squares made

--- react-app/src/components/Board/index.js
+++ react-app/src/components/Board/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Board = () => {
 
-  const rows = () => 'test';
+  const nums = [8, 7, 6, 5, 4, 3, 2, 1]
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+  // const [board, setBoard] = useState(
+  //   [
+  //     [1, 2, 3, 4],
+  //     [1, 2, 3, 4],
+  //     [1, 2, 3, 4],
+  //     [1, 2, 3, 4],
+  //   ]
+  // );
+
+  const rows = nums.map((num) => {
+    return (
+      <div className='row' key={num}>
+        {letters.map((letter) => {
+          return <div className='square'
+            key={letter + num}>{letter + num}</div>
+        })}
+      </div>
+    )
+  })
 
   return (
     <div id='board'>
