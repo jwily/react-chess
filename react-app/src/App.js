de77ecc515f05ea530500dc94b56c03f4c24b95e
jwily <johnlee1120@gmail.com>
2024-03-26 21:45:31 -0700

    use svgs as components to reduce load time

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,39 +3,39 @@ import { Route, Switch } from "react-router-dom";
 import Board from './components/Board';
 import Home from "./components/Home";
 
-import WhiteKing from './images/no_shadow/w_king_svg_NoShadow.svg'
-import WhiteQueen from './images/no_shadow/w_queen_svg_NoShadow.svg'
-import WhiteBishop from './images/no_shadow/w_bishop_svg_NoShadow.svg'
-import WhiteRook from './images/no_shadow/w_rook_svg_NoShadow.svg'
-import WhiteKnight from './images/no_shadow/w_knight_svg_NoShadow.svg'
-import WhitePawn from './images/no_shadow/w_pawn_svg_NoShadow.svg'
+// import WhiteKing from './images/no_shadow/w_king_svg_NoShadow.svg'
+// import WhiteQueen from './images/no_shadow/w_queen_svg_NoShadow.svg'
+// import WhiteBishop from './images/no_shadow/w_bishop_svg_NoShadow.svg'
+// import WhiteRook from './images/no_shadow/w_rook_svg_NoShadow.svg'
+// import WhiteKnight from './images/no_shadow/w_knight_svg_NoShadow.svg'
+// import WhitePawn from './images/no_shadow/w_pawn_svg_NoShadow.svg'
 
-import BlackKing from './images/no_shadow/b_king_svg_NoShadow.svg'
-import BlackQueen from './images/no_shadow/b_queen_svg_NoShadow.svg'
-import BlackBishop from './images/no_shadow/b_bishop_svg_NoShadow.svg'
-import BlackRook from './images/no_shadow/b_rook_svg_NoShadow.svg'
-import BlackKnight from './images/no_shadow/b_knight_svg_NoShadow.svg'
-import BlackPawn from './images/no_shadow/b_pawn_svg_NoShadow.svg'
+// import BlackKing from './images/no_shadow/b_king_svg_NoShadow.svg'
+// import BlackQueen from './images/no_shadow/b_queen_svg_NoShadow.svg'
+// import BlackBishop from './images/no_shadow/b_bishop_svg_NoShadow.svg'
+// import BlackRook from './images/no_shadow/b_rook_svg_NoShadow.svg'
+// import BlackKnight from './images/no_shadow/b_knight_svg_NoShadow.svg'
+// import BlackPawn from './images/no_shadow/b_pawn_svg_NoShadow.svg'
 
-const imagePaths = [
-  WhiteKing, WhiteQueen, WhiteBishop, WhiteRook, WhiteKnight, WhitePawn,
-  BlackKing, BlackQueen, BlackBishop, BlackRook, BlackKnight, BlackPawn
-]
+// const imagePaths = [
+//   WhiteKing, WhiteQueen, WhiteBishop, WhiteRook, WhiteKnight, WhitePawn,
+//   BlackKing, BlackQueen, BlackBishop, BlackRook, BlackKnight, BlackPawn
+// ]
 
 function App() {
 
-  const [imagesLoaded, setImagesLoaded] = useState({});
+  // const [imagesLoaded, setImagesLoaded] = useState({});
   const [freshGame, setFreshGame] = useState('');
 
-  useEffect(() => {
-    imagePaths.forEach(path => {
-      const img = new Image();
-      img.onload = () => setImagesLoaded(prev => ({ ...prev, [path]: true }));
-      img.src = path;
-    })
-  }, [])
+  // useEffect(() => {
+  //   imagePaths.forEach(path => {
+  //     const img = new Image();
+  //     img.onload = () => setImagesLoaded(prev => ({ ...prev, [path]: true }));
+  //     img.src = path;
+  //   })
+  // }, [])
 
-  if (!imagePaths.every(path => imagesLoaded[path])) return null;
+  // if (!imagePaths.every(path => imagesLoaded[path])) return null;
 
   return (
     <>
--- react-app/src/components/Board/Square.js
+++ react-app/src/components/Board/Square.js
@@ -3,6 +3,36 @@ import React, { useEffect, useState } from "react";
 import { pieceData, toRowCol } from '../../game-logic';
 import determineAnimation from "./animations";
 
+import { ReactComponent as WhiteKing } from '../../images/no_shadow/w_king_svg_NoShadow.svg'
+import { ReactComponent as WhiteQueen } from '../../images/no_shadow/w_queen_svg_NoShadow.svg'
+import { ReactComponent as WhiteBishop } from '../../images/no_shadow/w_bishop_svg_NoShadow.svg'
+import { ReactComponent as WhiteRook } from '../../images/no_shadow/w_rook_svg_NoShadow.svg'
+import { ReactComponent as WhiteKnight } from '../../images/no_shadow/w_knight_svg_NoShadow.svg'
+import { ReactComponent as WhitePawn } from '../../images/no_shadow/w_pawn_svg_NoShadow.svg'
+
+import { ReactComponent as BlackKing } from '../../images/no_shadow/b_king_svg_NoShadow.svg'
+import { ReactComponent as BlackQueen } from '../../images/no_shadow/b_queen_svg_NoShadow.svg'
+import { ReactComponent as BlackBishop } from '../../images/no_shadow/b_bishop_svg_NoShadow.svg'
+import { ReactComponent as BlackRook } from '../../images/no_shadow/b_rook_svg_NoShadow.svg'
+import { ReactComponent as BlackKnight } from '../../images/no_shadow/b_knight_svg_NoShadow.svg'
+import { ReactComponent as BlackPawn } from '../../images/no_shadow/b_pawn_svg_NoShadow.svg'
+
+const SVG_MAP = {
+  'K': WhiteKing,
+  'Q': WhiteQueen,
+  'B': WhiteBishop,
+  'R': WhiteRook,
+  'N': WhiteKnight,
+  'P': WhitePawn,
+  'k': BlackKing,
+  'q': BlackQueen,
+  'b': BlackBishop,
+  'r': BlackRook,
+  'n': BlackKnight,
+  'p': BlackPawn,
+  '_': null,
+}
+
 const animationClasses = [
   ' fade-in-v-fast',
   ' fade-in-fast',
@@ -26,6 +56,28 @@ const Square = React.memo(({ notation, piece, player, isEnPassantTarget,
     return () => clearTimeout(removeAnimation);
 
   }, [])
+
+  let PieceComponent = SVG_MAP[piece];
+
+  const determinePieceClass = () => {
+
+    let classString = '';
+
+    if (piece.toLowerCase() === piece) {
+      classString += 'black';
+    } else {
+      classString += 'white';
+    }
+
+    if (piece.toLowerCase() === 'p') {
+      classString += ' pawn';
+    } else {
+      classString += ' piece';
+    }
+
+    return classString
+
+  }
 
   const isAttackable = isPossible && (piece !== '_' || isEnPassantTarget);
 
@@ -57,12 +109,14 @@ const Square = React.memo(({ notation, piece, player, isEnPassantTarget,
       className={
         determineColor()
         + determineStatus()
-        + (piece !== '_' ? ` ${pieceData[piece]['player'] + ' ' + pieceData[piece]['name']}` : '')
+        // + (piece !== '_' ? ` ${pieceData[piece]['player'] + ' ' + pieceData[piece]['name']}` : '')
         + (animated ? animationClasses[determineAnimation(notation, fadeType)] : '')
         + determingCastlingDisplay()
         + (displayEnPassant ? ' transparent' : '')
       }
       id={notation} >
+      {PieceComponent && <PieceComponent
+        className={determinePieceClass(piece)} />}
     </span >
   )
 })
