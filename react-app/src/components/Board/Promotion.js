77fb52ed47b1330b590a4966efee62b582c401fe
jwily <johnlee1120@gmail.com>
2024-04-13 21:44:14 -0700

    render choices during promotion

--- react-app/src/components/Board/Promotion.js
+++ react-app/src/components/Board/Promotion.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 import './pieces.css';
+import { isWhite } from "../../game-logic";
 
 const Promotion = ({ player, data }) => {
 
+  const pieces = isWhite(player) ? ['Q', 'N', 'R', 'B'] : ['q', 'n', 'r', 'b'];
+
   return (
     <span className={`promotion-grid ${player}`}>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      {pieces.map(piece => {
+
+        const { color, name, image } = data[piece];
+        const SVG = image;
+
+        return (
+          <button>
+            <SVG className={color + ' ' + name} />
+          </button>
+        )
+      })}
     </span>
   )
 
